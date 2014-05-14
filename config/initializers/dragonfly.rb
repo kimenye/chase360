902c5f8fedc9e11f0302218c4a0b3aaf8e2b2422
Sir Bertly <sirbertly@touchinspiration.com>
2014-05-14 13:15:32 +0300

    initial active admin

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,34 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick
+
+  protect_from_dos_attacks true
+  secret "e8c16ce703f49e5e5cbf8f68c71633cb2f746ef11359156ee34a0202de20af92"
+
+  url_format "/media/:job/:name"
+
+  if Rails.env.production?
+    datastore :s3,
+    bucket_name: ENV["S3_BUCKET_NAME"],
+    access_key_id: ENV["S3_ACCESS_ID"],
+    secret_access_key: ENV["S3_ACCESS_KEY"]
+  else
+    datastore :file,
+      root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+      server_root: Rails.root.join('public')
+  end
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
+
+# Add model functionality
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend Dragonfly::Model
+  ActiveRecord::Base.extend Dragonfly::Model::Validations
+end
