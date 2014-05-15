a03a642358c3962a395900cac5fad76cb6745803
Sir Bertly <sirbertly@touchinspiration.com>
2014-05-15 11:03:46 +0300

    heroku changes

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Chase360
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w[active_admin.css active_admin.js print.css]
   end
 end
