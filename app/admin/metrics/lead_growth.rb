1330a7840e1004d2da16d29c075e7c1f265368f0
Trevor Kimenye <kimenye@gmail.com>
2014-06-25 14:24:08 +0300

    manually computing group by date

--- app/admin/metrics/lead_growth.rb
+++ app/admin/metrics/lead_growth.rb
@@ -1,11 +1,28 @@
 ActiveAdmin.register_page "Lead Growth"  do
 
-	# menu :parent => "Reports"
-	menu false
+	menu :parent => "Reports"
+	# menu false
 
   content do
-    @metric = Lead.group_by_day(:created_at, day_start: 2).count
+    # @metric = Lead.group_by_day(:created_at).count    
+    first_date = Lead.first.created_at.to_date
+    last_date = Lead.last.created_at.to_date
+    range = first_date..last_date
+    days = range.to_a
+    day_time = days.collect { |d| d.to_time }
+
+    
+    raw = Lead.all.group_by { |g| g.created_at.to_date }.collect { |group|  [group.first.to_time, group[1].length] }.to_h
+
+    day_time.each do |day|
+    	if !raw.has_key?(day)
+    		raw[day] = 0
+    	end
+    end
+    @metric = raw
+
     render :partial => 'metrics/line_chart', :locals => {:metric => @metric}
+
   end
 
 end  
