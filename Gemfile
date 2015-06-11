aa2a7af40afa020e8a90ae11bfebe991af654125
Trevor Kimenye <kimenye@gmail.com>
2015-06-11 11:46:35 +0300

    Finished home page

--- Gemfile
+++ Gemfile
@@ -14,14 +14,9 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-# gem 'turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
@@ -90,14 +85,5 @@ group :production do
   # Use mysql as the database for Active Record
   gem 'mysql2'
 end
-
-# Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.1.2'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
 
 ruby "2.1.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,72 +1,19 @@
 Chase360::Application.routes.draw do
 
   ActiveAdmin.routes(self)
+  
   match 'verify', to: 'sessions#create', as: 'verify', via: "post"
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'summary', to: 'companies#summary', as: 'company_summary'
   get 'company/:id/users', to:'users#relationship_officers',as:'relationship_officers'
-  get 'users/:id/feed', to: 'users#feed'
-  # root :to => "home#index"
-  root to: "admin/dashboard#index"
+  get 'users/:id/feed', to: 'users#feed'  
+  # root to: "admin/dashboard#index"
+  get '/admin', to: 'admin/dashboard#index'
   
   post "users/:id", to: 'users#update', as: 'update_user'
   post "leads/:id", to: 'leads#close', as: 'close_lead'
 
+  root to: "home#index"
 
   resources :companies, :leads, :users, :departments
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
