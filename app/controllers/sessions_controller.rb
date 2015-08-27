912402a252fbb6af89007d0b4f56fc9775519e2e
Trevor Kimenye <kimenye@gmail.com>
2015-08-27 08:42:02 +0300

    Fixed session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,13 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       render json: {user_id: user.id, user_email: user.email, user_role: user.role_name, message: "You are logged in", status: :success, setup: user.setup}
     else
-      render json: {message: "Email is invalid", status: :unprocessable_entity}
+      # check if the user is valid
+      if !User.is_allowed?(params[:email])
+        render json: {message: "Email is invalid", status: :unprocessable_entity}
+      else
+        user = User.create! email: params[:email], setup: false
+        render json: {user_id: user.id, user_email: user.email, user_role: user.role_name, message: "You are logged in", status: :success, setup: user.setup}
+      end
     end
   end
 
